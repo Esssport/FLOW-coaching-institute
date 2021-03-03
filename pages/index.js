c60ad851641f8f5efc3222d93827e08562ab91e3
Alireza Izadjoo <aizadjoo@jirnexu.com>
2021-03-03 16:46:40 -0500

    Update events tile

--- pages/index.js
+++ pages/index.js
@@ -68,34 +68,36 @@ export default function Home() {
               <div className="description">
                 <h1>UPCOMING<br />EVENTS</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi et, adipiscing ac feugiat fermentum. Arcu orci sodales nibh sit fermentum. Ut sed sed id pellentesque est rhoncus blandit.</p>
+                <div className="cta-orange"><a href="/">Go to event calendar &rarr;</a></div>
               </div>
               <div className="event-slider-container">
-
                 <CustomSlider tileType="event-tiles">
-
                   <div className="event-tile">
                     <div className="date-bar">
-                      <h1><span className="day-sub-section">08</span><br/><span className="month-sub-section">January</span></h1>
+                      <h1><span className="day-sub-section">08</span><br /><span className="month-sub-section">January</span></h1>
                       <a className="free-button red-button">Free</a>
                     </div>
                     <h1>Flow Global Talks</h1>
+                    <small>Webinar</small>
+                    <a className="learn-more-events">Learn more &rarr;</a>
+                    <div className="info">
+                      <div className="sponsor-wrapper">
+                        <small>sponsored by</small>
+                        <img src="/logo.svg" alt="sponsor logo"></img>
+                      </div>
+                      <div className="support-wrapper">
+                        <small>supported by</small>
+                        <h1>Emily Carr</h1>
+                        <h2>University of Art + Design</h2>
+                      </div>
+                    </div>
                   </div>
                 </CustomSlider>
-
-
-
               </div>
-
-
-
-
-
-
             </div>
-
-
-
-
+            
+            
+            
           </div>
         </div>
 
